cb61f3a8a890ea0288c6735b7eb950b07ade5dba
Arindam <arindam.g.ios@gmail.com>
2017-07-17 22:02:41 +0530

    Revert "adding submodule"
    This reverts commit acc9a483ebc4b9b46d1aa24d0d4c31da05e54fb0.

--- Pods/Target Support Files/LayoutKit/LayoutKit-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double LayoutKitVersionNumber;
-FOUNDATION_EXPORT const unsigned char LayoutKitVersionString[];
-
--- Pods/Target Support Files/Pods-NestedSections/Pods-NestedSections-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NestedSections : NSObject
-@end
-@implementation PodsDummy_Pods_NestedSections
-@end
